4043b5285a55a146c21b21f449b8d3227f572352
mohamed.elmarzgioui@gmail.com <mohamed.elmarzgioui@gmail.com@38dd782e-c320-bfd2-7297-567593b2e1f4>
2014-10-17 15:06:11 +0000

    policy client momo

--- ProjetForumClient/src/client/IServeurC.java
+++ ProjetForumClient/src/client/IServeurC.java
@@ -4,7 +4,7 @@ import java.rmi.RemoteException;
 import java.util.Set;
 
 public interface IServeurC {
-	public ISujetC obtientSujet(String s) throws RemoteException;
+	public ISujet obtientSujet(String s) throws RemoteException;
 	
 	public Set<String> ensembleSujets() throws RemoteException;
 }
