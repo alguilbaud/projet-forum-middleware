6d5ffaa18d9230808136e818944fa07c8c195b3d
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@38dd782e-c320-bfd2-7297-567593b2e1f4>
2014-12-15 13:44:57 +0000

    modification lancement

--- trunk/ProjetForumServeur/src/service/LancementServeur.java
+++ trunk/ProjetForumServeur/src/service/LancementServeur.java
@@ -6,34 +6,13 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class LancementServeur {
-
-	/*public static void main(String[] args) {
-		try {
-			Sujet sport = new Sujet("Sport");
-			Sujet cinema = new Sujet("Cinéma");
-			Sujet musique = new Sujet("Musique");
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		
-		//créer rmi
-	}*/
 	
 	public static void main(String[] args) {
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
         try {
         	LocateRegistry.createRegistry(8099);
-        	Sujet sujet = new Sujet("Sport");
-			Naming.rebind("rmi://localhost:8099/sujet", sujet);
-			System.out.println(sujet);
-            //IServeur stubServeur = (IServeur) UnicastRemoteObject.exportObject(serveur, 0);
-			//Registry registry = LocateRegistry.getRegistry();
-			//registry.rebind("Serveur", stubServeur);
-			//System.out.println("Chat bound");
+        	Serveur serveur = new Serveur();
+			Naming.rebind("rmi://localhost:8099/serveur", serveur);
+			System.out.println(serveur);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
