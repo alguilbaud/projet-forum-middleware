bcc2b19f3e3ce76c937cf7ee8d8e6f77ee6ff60d
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@38dd782e-c320-bfd2-7297-567593b2e1f4>
2014-12-15 14:45:47 +0000

    serveur v1 fonctionnelle

--- trunk/ProjetForumServeur/src/service/IServeur.java
+++ trunk/ProjetForumServeur/src/service/IServeur.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 import java.util.Set;
 
 public interface IServeur extends Remote{
-	public Sujet obtientSujet(String s) throws RemoteException;
+	public ISujet obtientSujet(String s) throws RemoteException;
 	
 	public Set<String> ensembleSujets() throws RemoteException;
 }
